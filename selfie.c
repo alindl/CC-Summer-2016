84ec6290006f36cfe17c7cdbea2fb926411b9efa
Andreas Lindlbauer <alindl@cosy.sbg.ac.at>
2016-04-29 11:22:07 +0200

    added the brackets to the chars and symbols

--- selfie.c
+++ selfie.c
@@ -140,6 +140,8 @@ int CHAR_EXCLAMATION  = '!';
 int CHAR_PERCENTAGE   = '%';
 int CHAR_SINGLEQUOTE  = 39; // ASCII code 39 = '
 int CHAR_DOUBLEQUOTE  = '"';
+int CHAR_LBRACKET     = '[';
+int CHAR_RBRACKET     = ']';
 
 int SIZEOFINT     = 4; // must be the same as WORDSIZE
 int SIZEOFINTSTAR = 4; // must be the same as WORDSIZE
@@ -277,6 +279,8 @@ int SYM_CHARACTER    = 26; // character
 int SYM_STRING       = 27; // string
 int SYM_LS           = 28; // <<
 int SYM_RS           = 29; // >>
+int SYM_LBRACKET     = 30; // [
+int SYM_RBRACKET     = 31; // ]
 
 int* SYMBOLS; // array of strings representing symbols
 
@@ -309,7 +313,7 @@ int  sourceFD   = 0;        // file descriptor of open source file
 // ------------------------- INITIALIZATION ------------------------
 
 void initScanner () {
-    SYMBOLS = malloc(32 * SIZEOFINTSTAR);
+    SYMBOLS = malloc(34 * SIZEOFINTSTAR);
 
     *(SYMBOLS + SYM_IDENTIFIER)   = (int) "identifier";
     *(SYMBOLS + SYM_INTEGER)      = (int) "integer";
@@ -341,6 +345,9 @@ void initScanner () {
     *(SYMBOLS + SYM_STRING)       = (int) "string";
     *(SYMBOLS + SYM_LS)           = (int) "<<";
     *(SYMBOLS + SYM_RS)           = (int) ">>";
+    *(SYMBOLS + SYM_LBRACKET)     = (int) "[";
+    *(SYMBOLS + SYM_RBRACKET)     = (int) "]";
+
 
     character = CHAR_EOF;
     symbol    = SYM_EOF;
